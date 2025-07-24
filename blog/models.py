9296cae85edbd60bd94bfd876f8b0be1bf767086
gabriel00000xp <gabrielviveiroscastillo2@gmail.com>
2025-07-24 18:00:19 -0400

    CambiosMenores

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE) 
+    
     def __str__(self):
         return self.title
