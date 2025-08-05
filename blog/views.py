9c7afa53cefa087930d8a1c40844b3a46e330abe
gabriel00000xp <gabrielviveiroscastillo2@gmail.com>
2025-08-05 17:53:12 -0400

    post_list_listo

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView   
+from .models import Post
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    
