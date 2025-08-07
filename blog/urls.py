e3a48a080441ed69a17897fd3fcd72ebe233be4f
gabriel00000xp <gabrielviveiroscastillo2@gmail.com>
2025-08-07 17:31:59 -0400

    agregado

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import PostListView
+from .views import PostListView, PostDetailView , PostCreateView
 from django.views.generic import DetailView, CreateView
 from .models import Post
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
-    path('post/<int:pk>/', DetailView.as_view(model=Post, template_name='post_detail.html'), name='post_detail'),
-    path('post/new/', CreateView.as_view(model=Post, template_name='post_new.html', fields=['title', 'content']), name='post_new'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_new'),
 ]
