d54f1abfcd05571aa8b8ff086a6c1dab49030642
gabriel00000xp <gabrielviveiroscastillo2@gmail.com>
2025-08-19 17:45:45 -0400

    permisos

--- blog/views.py
+++ blog/views.py
@@ -3,34 +3,35 @@ from django.views.generic import ListView, DetailView, DeleteView
 from  django.views.generic.edit import CreateView , UpdateView
 from .models import Post
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-class PostListView(ListView):
+class PostListView( ListView):
     model = Post
     template_name = 'post_list.html'
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'content', 'author']
 #
 #
-class PostEditView(UpdateView):
+class PostEditView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'content']
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'updates-post.html'
     fields = ['title', 'content']
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('post_list')
